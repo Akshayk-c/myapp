ae3f6f1939bb2e30d69627e3b06263342f029cfd
Aks1shayy <akshaykumarc666@gmail.com>
2022-10-06 20:34:12 +0530

    use

--- front-end/src/components/UserView.js
+++ front-end/src/components/UserView.js
@@ -2,38 +2,49 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
-function  UserView ({mutate}) {
+function UserView({ mutate }) {
   const [users, setUsers] = useState([])
-  useEffect( () => {
-      axios.get('http://localhost:5000/user')
+  useEffect(() => {
+    axios.get('http://localhost:5000/user')
       .then(res => {
         setUsers(res.data)
       })
       .catch(err => {
         console.log(err)
       })
-      
-  },[mutate])
-  
+
+  }, [mutate])
+
   return (
     <>
-    <label style={{fontSize:'large'}}>USER DETAILS</label>
-    <table style={{border: 'double'}} className="col-xs-12 col-sm-6 col-md-8">
-      <thead><tr>
+      <label style={{ fontSize: 'large' }}>USER DETAILS</label>
+      <table style={{ border: 'double' }} className="col-xs-12 col-sm-6 col-md-8">
+        <thead><tr>
           <th>First name</th>
           <th>Last name</th>
           <th>Email id</th>
-          <th>Password</th></tr>
-          </thead>
-          <tbody>
+          <th>Password</th>
+          <th>Edit</th>
+          <th>Delete</th></tr>
+        </thead>
+        <tbody>
           {
-          users.map((data) => (
-             <tr key={data._id}><td>{data.fname}</td>
-            <td>{data.lname}</td><td>{data.email}</td><td>{data.password}</td></tr>
-            ))  
-        }
+            users.map((data) => (
+              <tr key={data._id}>
+                <td>{data.fname}</td>
+                <td>{data.lname}</td>
+                <td>{data.email}</td>
+                <td>{data.password}</td>
+                <td>
+                <input type='button' value='Edit' />
+                </td>
+                <td>
+                <input type='button' value='Delete' />
+                </td></tr>
+            ))
+          }
         </tbody>
-    </table> 
+      </table>
     </>
   )
 }
