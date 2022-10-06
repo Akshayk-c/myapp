b3c8a2ca046a3f5cc332fb69ed29aa7d6e799322
Aks1shayy <akshaykumarc666@gmail.com>
2022-10-06 20:15:47 +0530

    html

--- front-end/src/components/UserView.js
+++ front-end/src/components/UserView.js
@@ -17,16 +17,18 @@ function  UserView ({mutate}) {
   
   return (
      
-    <table >
-      <tr>
+    <table style={{border: 'double'}} class="col-xs-12 col-sm-6 col-md-8">
+      <label style={{fontSize:'large'}}>USER DETAILS</label>
+      <tr style={{width: '50%'}}>
           <th>First name</th>
           <th>Last name</th>
           <th>Email id</th>
+          <th>Password</th>
           </tr>
           {
           users.map((data) => (
              <tr key={data._id}><td>{data.fname}</td>
-            <td>{data.lname}</td><td>{data.email}</td></tr>
+            <td>{data.lname}</td><td>{data.email}</td><td>{data.password}</td></tr>
             ))  
         }
     </table> 
