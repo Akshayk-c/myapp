74cd5d76379c90d9afc85b06945d9183910b8f96
Aks1shayy <akshaykumarc666@gmail.com>
2022-10-07 16:58:48 +0530

    form removed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
-import UserForm from './components/UserForm';
-
+import React from 'react';
 import UserView from './components/UserView';
 
 function App() {
-  const [mutate,setMutate]=useState("")
   return (
     <>
-       <UserForm  setMutate={setMutate}/>
-        <UserView mutate={mutate}/>
+       
+        <UserView/>
     </>
   );
 }
--- front-end/src/components/UserView.js
+++ front-end/src/components/UserView.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
-function UserView({ mutate }) {
-  
+function UserView() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [users, setUsers] = useState([])
   const [render, setRender] = useState([])
-   useEffect(() => {
+
+  useEffect(() => {
     axios.get('http://localhost:5000/user')
       .then(res => {
         setUsers(res.data)
@@ -14,18 +18,72 @@ function UserView({ mutate }) {
       .catch(err => {
         console.log(err)
       })
-  }, [mutate,render])
+  }, [render])
 
-
-  function deleteUser(id,e){
+  const addUser = async (e) => {
+    e.preventDefault()
+    try {
+      const resp = await axios.post("http://localhost:5000/user", {
+        email: email,
+        lname: lname,
+        fname: fname,
+        password: password,
+      });
+      if (resp.status === 200) alert("User Added successfuly")
+    } catch (error) {
+      console.log(error.resp);
+    }
+    setRender(Math.random)
+  }
+  function deleteUser(id, e) {
     console.log(id)
     axios.delete(`http://localhost:5000/user/${id}`)
-    .then(res=>alert("User deleted successfuly"))
-    .catch(error=>{console.log(error)})
+      .then(res => alert("User deleted successfuly"))
+      .catch(error => { console.log(error) })
     setRender(Math.random)
   }
   return (
     <>
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            name="lname"
+            onChange={(e) => setFname(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Last name"
+            name="fname"
+            onChange={(e) => setLname(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={addUser}>
+            Add
+          </button>
+          <button type="reset">Reset</button>
+        </div>
+      </div>
       <label style={{ fontSize: 'large' }}>USER DETAILS</label>
       <table style={{ border: 'double' }} >
         <thead><tr>
@@ -45,10 +103,10 @@ function UserView({ mutate }) {
                 <td>{data.email}</td>
                 <td>{data.password}</td>
                 <td>
-                <button>Edit</button>
+                  <button>Edit</button>
                 </td>
                 <td>
-                <button onClick={(e)=>deleteUser(data._id,e)}>Delete</button>
+                  <button onClick={(e) => deleteUser(data._id, e)}>Delete</button>
                 </td></tr>
             ))
           }
