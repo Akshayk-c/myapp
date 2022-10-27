b4b58eed8bc4b8a922b2c33d4d25e9e8d33bed47
Aks1shayy <akshaykumarc666@gmail.com>
2022-10-27 13:17:35 +0530

    up

--- front-end/src/components/login.js
+++ front-end/src/components/login.js
@@ -6,11 +6,15 @@ import './styles/style.css'
 function login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [emailerr, setEmailErr] = useState('')
+  const [passworderr, setPasswordErr] = useState('')
   const navigate = useNavigate();
 
 
   async function addUser(e) {
     e.preventDefault()
+    setEmailErr('')
+    setPasswordErr('')
     try {
       const resp = await axios.post("http://localhost:5000/user/login", {
         email: email,
@@ -24,91 +28,101 @@ function login() {
 
       }
     } catch (error) {
-      alert(error.response.data)
-      navigate('/');
-      console.log(error);
+      if (error.response.data === "User email doesn't exist") setEmailErr('Invalid email')
+      if (error.response.data === "wrong password") setPasswordErr('Invalid password')
+      
+
+      console.log(error.response.data);
     }
+  }
+  const resetData=()=>{
+    setEmail('')
+    setPassword('')
+    setEmailErr('')
+    setPasswordErr('')
   }
 
   return (
-    
-      <div class='d-flex p-2 justify-content-center align-items-center' style={{ height: 100 + 'vh' }}>
 
-        <form onSubmit={addUser} >
-          <div class="outerDiv ">
+    <div class='d-flex p-2 justify-content-center align-items-center' style={{ height: 100 + 'vh' }}>
 
-            <div class="card px-1 py-4">
+      <form onSubmit={addUser} >
+        <div class="outerDiv ">
 
-              <div class="card-body">
+          <div class="card px-1 py-4">
+
+            <div class="card-body">
+
+              <div>
+
+                <h1 class="headerText">Enter Login Details</h1>
+
+              </div>
+
+
+              <div class="row">
+
+                <div class="col-sm-12">
+
+                  <div class="form-group">
+                  
+                    <input class={`form-control ${emailerr ? 'is-invalid ': '' }`} id="nameTxt" type="email" placeholder="Email" value={email} onChange={(e) => {setEmail(e.target.value)
+                    setEmailErr('')}} />
+                    <span class='text-danger'>{emailerr}</span>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div class="row">
+
+                <div class="col-sm-12">
+
+                  <div class="form-group">
+
+                    <input class={`form-control ${passworderr ? 'is-invalid ': '' }`} id="dobTxt" type="password" placeholder="password" value={password}  onChange={(e) => {setPassword(e.target.value)
+                    setPasswordErr('')}} />
+
+                    <span class='text-danger'>{passworderr}</span>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div class="d-flex  text-center px-5 mx-3 d-flex justify-content-evenly" >
 
                 <div>
 
-                  <h1 class="headerText">Enter Login Details</h1>
+                  <button class="btn btn-success confirm-button w-100" >Save</button>
 
                 </div>
 
+                <div>
 
-                <div class="row">
-
-                  <div class="col-sm-12">
-
-                    <div class="form-group">
-
-                      <input class="form-control nameCss" id="nameTxt" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-
-
-                    </div>
-
-                  </div>
+                  <button class="btn btn-warning  confirm-button w-100" type="reset" onClick={resetData}>reset</button>
 
                 </div>
 
-                <div class="row">
-
-                  <div class="col-sm-12">
-
-                    <div class="form-group">
-
-                      <input class="form-control dobCss" id="dobTxt" type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
-
-
-
-                    </div>
-
-                  </div>
-
-                </div>
-
-                <div class="d-flex  text-center px-5 mx-3 d-flex justify-content-evenly" >
-
-                  <div>
-
-                    <button class="btn btn-success confirm-button w-100" >Save</button>
-
-                  </div>
-
-                  <div>
-
-                    <button class="btn btn-warning  confirm-button w-100" type="reset" >reset</button>
-
-                  </div>
-
-                </div>
-                <div className="text-center my-4">
+              </div>
+              <div className="text-center my-4">
                 <Link to={`/register`} className='links' >New user ? Sign up</Link>
-                </div>
-             
               </div>
 
             </div>
 
-
-
           </div>
-        </form>
-      </div>
 
-   
+
+
+        </div>
+      </form>
+    </div>
+
+
   )
 }
 
